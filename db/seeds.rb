018502a32ae5d31f71c7691e166b903a386119f9
Stefan Sarow <stefan@pixelaffairs.com>
2020-05-22 13:28:01 +0200

    root path changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+puts 'Burning down old database...'
+Ingredient.destroy_all
+puts 'Stupid marketing guys create new ingredients...'
+ingredients = JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
+ingredients['drinks'].each do |x|
+  Ingredient.create(name: x['strIngredient1'])
+  puts "Ingredient #{x['strIngredient1']} created"
+end
